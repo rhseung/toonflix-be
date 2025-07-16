44b806fc7bf9acbfcea455a08c63d90ac0cbdf70
Hyunseung Ryu <rhseungg@gmail.com>
2025-07-17 00:02:08 +0900

    fix: use custom js and css for swagger

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,21 @@ async function bootstrap() {
     }),
   );
 
-  SwaggerModule.setup('api', app, () =>
-    SwaggerModule.createDocument(app, config),
+  SwaggerModule.setup(
+    'api',
+    app,
+    () => SwaggerModule.createDocument(app, config),
+    {
+      customJs: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+      ],
+      customCssUrl: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+      ],
+    },
   );
 
   await app.listen(process.env.PORT ?? 3000);
