4c5b9a7ef689bc8f4bf4ca7d8c30507cae82e3fa
Hyunseung Ryu <rhseungg@gmail.com>
2025-07-20 02:18:09 +0900

    refactor: change table schema

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -2,10 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreatePostDto {
-  @ApiProperty({ example: 'Post Title' })
-  @IsString()
-  title: string;
-
   @ApiProperty({ example: 'Post Content' })
   @IsString()
   content: string;
